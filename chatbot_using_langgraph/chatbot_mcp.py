a7ad782b352e83e5a2acc799698a1245afd47473
Avanindra Bose <avanindrabose0218@gmail.com>
2026-08-27 18:35:25 +0530

    Added Chatbot Using Local MCP Server

---
+++ chatbot_using_langgraph/chatbot_mcp.py
@@ -0,0 +1,78 @@
+import asyncio
+from langgraph.graph import StateGraph, START
+from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+from langchain_community.tools import DuckDuckGoSearchRun
+from typing import TypedDict,Annotated
+from langchain_core.messages import BaseMessage, HumanMessage
+from langgraph.graph.message import add_messages
+from langgraph.prebuilt import ToolNode, tools_condition
+from langchain_core.tools import tool
+from langchain_mcp_adapters.client import MultiServerMCPClient
+
+load_dotenv()  # Load environment variables from .env file
+
+llm = ChatOpenAI(model="gpt-5")
+
+SERVERS = {
+    "local_demo": {
+        "transport": "stdio",
+         "command": "uv",
+        "args": [
+            "run",
+            "fastmcp",
+            "run",
+            r"C:\Users\avanindra Bose\MCP - Learning\mcp_server_local_demo.py"
+        ]
+    }
+}
+
+client = MultiServerMCPClient(SERVERS)
+
+# state
+class ChatState(TypedDict):
+    messages: Annotated[list[BaseMessage], add_messages]
+
+
+async def build_graph():
+
+    tools = await client.get_tools()
+
+    print(tools)
+
+    llm_with_tools = llm.bind_tools(tools=tools)
+    # All the custom nodes in lang graph needs to be converted into async using async def.
+    async def chat_node(state: ChatState):
+
+        messages = state["messages"]
+        response = await llm_with_tools.ainvoke(messages)
+        return {'messages': [response]}
+    
+    # tool node by default operates asynchronously
+    tool_node = ToolNode(tools)
+
+    # defining graph and nodes
+    graph = StateGraph(ChatState)
+
+    graph.add_node("chat_node", chat_node)
+    graph.add_node("tools", tool_node)
+
+    # defining graph connections
+    graph.add_edge(START, "chat_node")
+    graph.add_conditional_edges("chat_node", tools_condition)
+    graph.add_edge("tools", "chat_node")
+
+    chatbot = graph.compile()
+
+    return chatbot
+
+async def main():
+
+    chatbot = await build_graph()
+
+    result = await chatbot.ainvoke({"messages": [HumanMessage(content="Roll 4 six-sided dice and generate the result")]})
+
+    print(result['messages'][-1].content)
+
+if __name__ == "__main__":
+    asyncio.run(main())
